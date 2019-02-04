21230bcb6ee36be60b6226c051ac9104dacdcc87
rc0033939 <rc0033939@CRO-24-17>
2019-02-04 15:41:51 +0000

    changed hello

--- hello world/Form1.Designer.cs
+++ hello world/Form1.Designer.cs
@@ -37,9 +37,8 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "hello";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
