78d259bc4e88dbc4524a70799f16a9700400b7ce
rc0033939 <rc0033939@CRO-24-17>
2019-02-05 09:49:22 +0000

    cfghujhdfhd geetewtery

--- hello world/Form1.cs
+++ hello world/Form1.cs
@@ -24,12 +24,11 @@ namespace hello_world
 
         private void txtbxname_TextChanged(object sender, EventArgs e)
         {
-
+          
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            MessageBox.Show("Goodbye" + txtBxName.Text);
             MessageBox.Show("Goodbye" + txtBxName.Text);
         }
     }
